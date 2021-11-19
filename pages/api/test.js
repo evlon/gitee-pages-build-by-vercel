07ee554213ebc333a1586bfda3904f2ce185bd17
niukunliang <niukunliang@cmict.chinamobile.com>
2021-11-19 15:00:18 +0800

    test

--- pages/api/test.js
+++ pages/api/test.js
@@ -1,7 +1,3 @@
-//const fetch = require('node-fetch');
-import buildPage from 'gitee-pages-build';
-// import { NextRequest } from '_next@12.0.1@next/dist/server/web/spec-extension/request';
-
 export default async function handler(req, res) {
   //let resp = await fetch("https://www.baidu.com");
   //let html = await resp.text();
