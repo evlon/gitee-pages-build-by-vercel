e26d3ba29743038bb81fc193cf03978c6dd05247
evlon <liamgnc@gmail.com>
2021-11-27 13:19:14 +0000

    fix bugs

--- pages/api/pagebuild.js
+++ pages/api/pagebuild.js
@@ -5,37 +5,48 @@ import  URLSearchParams  from 'url-search-params'
 let actionsmap = {};
 
 export default async function handler(req, res) {
-
   let giteeToken = req.headers["x-gitee-token"];
-  let giteeTimestamp = req.headers["x-gitee-timestamp"];
-  let giteeEvent = req.headers["x-gitee-event"];
-
-
   if (giteeToken != process.env.X_GITEE_TOKEN) {
     res.status(403).json({ msg: 'access not allowd.' })
     return;
   }
-  let repo = req.body.repository.path;
-  //更新环境变量
-  process.env.GITEE_REPO = repo;
-
   if (process.env.GITEE_USERNAME && process.env.GITEE_PASSWORD && process.env.GITEE_REPO) {
-    addAction(1, async state=>{
-      return { stacks: [{msg:'init api', time :new Date()}] };
-    })
 
-    addAction(2, async state=>{
-      let loginResult = await buildPage.gitee_login_with_obj_cookie();
-      state.stacks.push({msg:'gitee_login_with_obj_cookie', time :new Date(), result:loginResult});
-      return state;
-    })
+    let repo = req.body.repository.path;
+    let branch  = req.body.ref.split('/').pop();
+    if(branch == process.env.GITEE_BRANCH){
+      res.status(200).json({ msg: 'skip branch ' + branch })
+      return ;
+    }
+
+    if(req.method == "POST"){      
+      let giteeTimestamp = req.headers["x-gitee-timestamp"];
+      let giteeEvent = req.headers["x-gitee-event"];
     
-    addAction(3, async state=>{
-      let buildResult = await buildPage.pagebuild_with_obj_cookie();
-      state.stacks.push({msg:'pagebuild_with_obj_cookie', time :new Date(), result:buildResult});
-      console.log('build success.')
-      return state;     
-    })   
+      addAction(1, async state=>{
+        return {args: {giteeTimestamp, giteeEvent, repo, branch}, stacks: [{msg:'init api', time :new Date()}] };
+      })
+    }
+    else  if(req.method == "GET"){
+
+      addAction(2, async state=>{
+         //更新环境变量
+        process.env.GITEE_REPO = state.args.repo;
+        let loginResult = await buildPage.gitee_login_with_obj_cookie();
+        state.stacks.push({msg:'gitee_login_with_obj_cookie', time :new Date(), result:loginResult});
+        return state;
+      })
+      
+      addAction(3, async state=>{
+        //更新环境变量
+        process.env.GITEE_REPO = state.args.repo;
+        let buildResult = await buildPage.pagebuild_with_obj_cookie();
+        state.stacks.push({msg:'pagebuild_with_obj_cookie', time :new Date(), result:buildResult});
+        console.log('build success.')
+        return state;     
+      })   
+    }
+   
   }
   else {
     res.status(200).json({
@@ -48,6 +59,7 @@ export default async function handler(req, res) {
     GITEE_HTTPS=1
     GITEE_GIST_TOKEN=token_string
     GITEE_GIST_ID=gist_id
+    API_PAGE_BUILD_SELF_URI=https://xxxxxxx/api/pagebuild
     `})
   }
 
@@ -99,6 +111,11 @@ async function doNext(step,state){
   let query = new URLSearchParams({ step:step , state: JSON.stringify(state)}).toString();
   let queryUri = url + '?' + query;
   console.log('doNext ', queryUri, step, state);
-  fetch(queryUri)
+  fetch(queryUri,{
+    method:"GET",
+    headers:{
+      "x-gitee-token": process.env.X_GITEE_TOKEN
+    }
+  })
   await sleep(1000);
 }
