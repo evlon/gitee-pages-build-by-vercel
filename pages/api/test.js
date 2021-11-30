c932fbfa027199b0c7b22d222d43f44fc5e6918d
evlon <liamgnc@gmail.com>
2021-11-30 12:25:17 +0000

    123456

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,6 +3,6 @@ export default async function handler(req, res) {
   //let html = await resp.text();
   console.log(req);
   console.log(req.body);
-  res.status(200).json({ body: req.body})
+  res.status(200).json({ body: '1234567890'})
 }
 
