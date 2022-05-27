165e4e67c8418a62de00722b017d91f17f743093
evlon <liamgnc@gmail.com>
2022-05-27 10:49:09 +0800

    add

--- pages/api/startbuild.js
+++ pages/api/startbuild.js
@@ -2,9 +2,53 @@ import buildPage from 'gitee-pages-build';
 import fetch from 'node-fetch'
 import sleep from 'sleep-anywhere'
 import URLSearchParams from 'url-search-params'
+import Github from '../src/github'
+
 let GiteePage = buildPage.GiteePage;
 let gist = buildPage.giteeGistSync
 let actionsmap = {};
+
+
+async function uploadToGithub(owner, token, repo, gitfile,base64Content, commitMsg){
+    //完整链接
+    let uri = 'https://api.github.com/repos/' + owner + '/' + repo + '/contents/' + gitfile;
+
+    //调整头
+    let outBody, outStatus = 204,  reqHeaders = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
+        "Authorization":"token " + token,
+        "Content-Type": "application/json"
+    };
+
+    let res = await fetch(uri, {
+        timeout: 50000,
+        method: "PUT",
+        body: JSON.stringify({
+            message: commitMsg || 'put file by api',
+            committer : {
+                name: "committer",
+                email:"committer@github.com"
+            },
+            content: base64Content
+        }),
+        headers:  reqHeaders
+    });
+
+    //成功
+    if (res.status == 201) { //422 created?
+        //let rj = await res.json();
+        //outBody = JSON.stringify(rj);
+        return {success: true, msg: 'created'};
+    }
+    else if(res.status == 422){
+       // let rj = await res.json();
+        //outBody = JSON.stringify(rj);
+        return {success: true, msg: 'exists' };
+    } else {
+        outBody = res.body;
+        return {success: false, msg: res.status + ':' + res.statusText, outBody:outBody };
+    }
+}
 
 export default async function handler(req, res) {
   if (req.method != "POST") {
@@ -20,8 +64,7 @@ export default async function handler(req, res) {
 
 
   if (process.env.GITEE_USERNAME && process.env.GITEE_PASSWORD && process.env.GITEE_REPO) {
-    //User-Agent: git-oschina-hook // req.headers['user-agent'] == "git-oschina-hook" &&
-     
+    //User-Agent: git-oschina-hook // req.headers['user-agent'] == "git-oschina-hook" &&     
     let repo = req.body.repository.path;
     let branch = req.body.ref.split('/').pop();
     if (branch != process.env.GITEE_BRANCH) {
@@ -36,8 +79,15 @@ export default async function handler(req, res) {
         gituser:process.env.GITEE_USERNAME , gitpwd: process.env.GITEE_PASSWORD, gitdir: process.env.GITEE_DIRECTORY, 
         gisttoken: process.env.GITEE_GIST_TOKEN,
         gistid: process.env.GITEE_GIST_ID };
-        
-    const syncResult = await gist.save_obj(process.env.GITEE_GIST_TOKEN, process.env.GITEE_GIST_ID, "json_build.mem", jsonObject)
+
+    const syncResult = await gist.save_obj(process.env.GITEE_GIST_TOKEN, process.env.GITEE_GIST_ID, "json_build.mem", jsonObject);
+  
+    if(process.env.GITHUB_USERNAME && process.env.GITHUB_TOKEN && process.env.GITHUB_REPO){
+        let gitfile = process.env.GITEE_USERNAME + '/' + process.env.GITEE_REPO + new Date().valueOf() + ".md"
+        let gitfileContent = process.env.GITEE_USERNAME + '/' + process.env.GITEE_REPO;
+        let base64Content = Buffer.from(gitfileContent,'utf8').toString('base64');
+        await uploadToGithub(process.env.GITHUB_USERNAME,process.env.GITHUB_TOKEN, process.env.GITHUB_REPO,gitfile,base64Content);
+    }
   }
   else {
     res.status(200).json({
